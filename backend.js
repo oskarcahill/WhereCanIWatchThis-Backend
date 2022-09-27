2c85e106056404cc2f6e772489f938e0e6894b1d
ocahill <ocahill@tcd.ie>
2022-09-27 13:03:20 +0100

    modified to allow for tv show querying

--- backend.js
+++ backend.js
@@ -12,33 +12,55 @@ async function platformFinder(country,media,name){
     const rent = []
     const buy = []
     const url = `https://www.justwatch.com/${country}/${media}/${name}`;
+
+    if(media == "tv-series"){
+        console.log("TV")
         await axios.get(url).then((response) => {
             const html_data = response.data;
-            console.log(html_data)
             const $ = cheerio.load(html_data);
-            
+                
             //getting the streaming icons 
-            console.log("******STREAMING******")
             $(".price-comparison__grid__row.price-comparison__grid__row--stream.price-comparison__grid__row--block").children().last().children().each(function() {
-                console.log($(this).find('img').attr('src'))
-                stream.push($(this).find('img').attr('src'))
-                })
-            
+                    stream.push($(this).find('img').attr('data-src'))
+            })
+                
             //getting the renting icons 
-            console.log("******RENTING******")
             $(".price-comparison__grid__row.price-comparison__grid__row--rent.price-comparison__grid__row--block").children().last().children().each(function() {
-                console.log($(this).find('img').attr('data-src'))
                 rent.push($(this).find('img').attr('data-src'))
-                })
+            })
 
             //getting the buying icons 
-            console.log("********BUYING*******")
             $(".price-comparison__grid__row.price-comparison__grid__row--buy.price-comparison__grid__row--block").children().last().children().each(function() {
-                console.log($(this).find('img').attr('data-src'))
                 buy.push($(this).find('img').attr('data-src'))
-                })
+            })
 
     });
+
+    }
+    else if(media == "movie"){
+        console.log("MOVIE")
+        await axios.get(url).then((response) => {
+            const html_data = response.data;
+            const $ = cheerio.load(html_data);
+                
+            //getting the streaming icons 
+            $(".price-comparison__grid__row.price-comparison__grid__row--stream.price-comparison__grid__row--block").children().last().children().each(function() {
+                    stream.push($(this).find('img').attr('src'))
+            })
+                
+            //getting the renting icons 
+            $(".price-comparison__grid__row.price-comparison__grid__row--rent.price-comparison__grid__row--block").children().last().children().each(function() {
+                rent.push($(this).find('img').attr('data-src'))
+            })
+
+            //getting the buying icons 
+            $(".price-comparison__grid__row.price-comparison__grid__row--buy.price-comparison__grid__row--block").children().last().children().each(function() {
+                buy.push($(this).find('img').attr('data-src'))
+            })
+
+    });
+
+}
     platforms.push(stream);
     platforms.push(rent)
     platforms.push(buy)
